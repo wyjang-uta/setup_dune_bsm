d61c8e2cf84a49c442d242c31f0d89982fabbf01
Wooyoung Jang <wyjang@thinkpadp53.thinkpadp53>
2021-10-11 23:40:15 -0500

    GLoBES is disabled for LDM.

--- ldm.sh
+++ ldm.sh
@@ -28,18 +28,18 @@ setup gsl v2_5 -q prof
 
 echo "Setting up GSL2.5 ... DONE"
 
-echo "Setting up GLoBES 3.0.11 ..."
-
-export GLOBESDIR=/dune/app/users/wyjang/dune_bsm/globes/3.0.11
-echo "GLOBESDIR=$GLOBESDIR"
-export GLOBESBIN=$GLOBESDIR/bin
-echo "GLOBESBIN=$GLOBESBIN"
-export PATH=$PATH:$GLOBESBIN
-export GLOBESLIB=$GLOBESDIR/lib
-echo "GLOBESLIB=$GLOBESLIB"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOBESLIB
-
-echo "Setting up GLoBES 3.0.11 ... DONE"
+#echo "Setting up GLoBES 3.0.11 ..."
+#
+#export GLOBESDIR=/dune/app/users/wyjang/dune_bsm/globes/3.0.11
+#echo "GLOBESDIR=$GLOBESDIR"
+#export GLOBESBIN=$GLOBESDIR/bin
+#echo "GLOBESBIN=$GLOBESBIN"
+#export PATH=$PATH:$GLOBESBIN
+#export GLOBESLIB=$GLOBESDIR/lib
+#echo "GLOBESLIB=$GLOBESLIB"
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOBESLIB
+#
+#echo "Setting up GLoBES 3.0.11 ... DONE"
 
 echo "Setting up GENIE ..."
 
