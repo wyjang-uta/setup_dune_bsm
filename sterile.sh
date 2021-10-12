fefa34add7e51ba7761f5c81cde7af473b0af8ba
Wooyoung Jang <wyjang@thinkpadp53.thinkpadp53>
2021-10-12 14:58:40 -0500

    basic software support implemented for Sterile/LED/Nutau analysis.

--- sterile.sh
+++ sterile.sh
@@ -1,1 +1,31 @@
+# LED/Sterile/Nu_tau analysis requirement summary
+# (1) GSL v2.5
+# (2) GLoBES v3.2.18
+# (3) edepsim 3.1.0
+# (4) GiBUU 2019
+
 echo "Enabling Sterile Neutrino environment ..."
+echo "Initialize ..."
+unset GSLDIR
+unset GSLBIN
+unset GSLLIB
+unset GLOBESDIR
+unset GLOBESBIN
+unset GLOBESLIB
+
+echo "Setting up GSL2.5 ..."
+setup gsl v2_5 -q prof
+echo "Setting up GSL2.5 ... DONE"
+
+echo "Setting up GLoBES 3.0.11 ..."
+
+export GLOBESDIR=/dune/app/users/wyjang/dune_bsm/globes/3.0.11
+echo "GLOBESDIR=$GLOBESDIR"
+export GLOBESBIN=$GLOBESDIR/bin
+echo "GLOBESBIN=$GLOBESBIN"
+export PATH=$PATH:$GLOBESBIN
+export GLOBESLIB=$GLOBESDIR/lib
+echo "GLOBESLIB=$GLOBESLIB"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOBESLIB
+
+echo "Setting up GLoBES 3.0.11 ... DONE"
